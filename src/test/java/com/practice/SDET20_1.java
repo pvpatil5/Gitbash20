ec1e984e3abfc0acc7a7153fc623917c5e3b177d
pvpatil5 <pvpatil123@yahoo.com>
2021-08-30 11:38:32 +0530

    New Branch Created

---
+++ src/test/java/com/practice/SDET20_1.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class SDET20_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is SDET20 branch class 1");
+	}
+
+}
