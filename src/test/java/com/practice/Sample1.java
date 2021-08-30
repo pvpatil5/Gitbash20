0fe83834942c6f107d2350be8296d33464103b37
pvpatil5 <pvpatil123@yahoo.com>
2021-08-30 11:17:42 +0530

    This is my first commit

---
+++ src/test/java/com/practice/Sample1.java
@@ -0,0 +1,9 @@
+package com.practice;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is sample 1 class");
+	}
+
+}
---
+++ src/test/java/com/practice/Sample2.java
@@ -0,0 +1,9 @@
+package com.practice;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is sample 2 class");
+	}
+
+}
